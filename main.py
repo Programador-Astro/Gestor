b72c9d2a4245b233c886230bff1044d6401d555e
Programador-Astro <programadorastro@gmail.com>
2025-04-04 00:01:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,15 @@ from app.db import db
 import os
 
 
-
-
-#Imports para o register
+#Imports para o register dos Blue Prints
 from app.routes.auth import auth_bp
 from app.routes.gerencia import gerencia_bp
 from app.routes.estoque import estoque_bp
 from app.routes.logistica import logistica_bp
 from app.routes.financeiro import financeiro_bp
-#Tela raiz
 
 
+#Viwe raiz
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
@@ -33,49 +31,19 @@ def index():
         return redirect(url_for('auth.root',user=user ,pwd=pwd))
 
 
-#Rota que deve ser apagada
-@app.route('/register', methods=['get', 'post'])   
-def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    
-    if request.method == 'POST':
-        usuario = request.form['usuario']
-        pwd = request.form['pwd']
-        email = request.form['email']
-        cargo = request.form['cargo']
 
-        novo_usuario = User(user=usuario, pwd=pwd, email=email, cargo=cargo)
-        db.session.add(novo_usuario)
-        db.session.commit()
-        print(session)
-        return f'{usuario} {pwd}'
-
-#Register
+#Register Blue Prints
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(gerencia_bp, url_prefix='/gerencia')
 app.register_blueprint(estoque_bp, url_prefix='/estoque')
 app.register_blueprint(logistica_bp, url_prefix='/logistica')
 app.register_blueprint(financeiro_bp, url_prefix ='/financeiro')
 
-def criar_schema(nome_schema):
-    with app.app_context():
-        db.session.execute(text(f"CREATE SCHEMA IF NOT EXISTS {nome_schema};"))
-        db.session.commit()
-
 
 if __name__ == "__main__":
-    """with app.app_context():
-        #criar_schema('estoque')
-        db.create_all()
-    app.run()
-    """
     from threading import Thread
-    
     def run():
-        debug = True
         app.run()
-
     thread = Thread(target=run)
     thread.start()
     
