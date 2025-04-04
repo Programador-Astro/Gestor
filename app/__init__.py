16b93b8ed1ea46aed45390af4b723951c453fa6f
Programador-Astro <programadorastro@gmail.com>
2025-04-03 23:59:37 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,46 +10,31 @@ from app.models.estoque_model import *
 app = Flask(__name__)
 app.secret_key = '1223absc'
 lm = LoginManager(app)
-
-
-#Variavel AWS
-"""
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/grupo_mar'
 app.config['SQLALCHEMY_BINDS'] = {'estoque_producao':'mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/estoque_producao'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:328473@localhost/grupo_mar'
-app.config['SQLALCHEMY_BINDS'] = {
-    'estoque_producao':'mysql+pymysql://root:328473@localhost/estoque_producao',
-    #'grupo_mar1': 'mysql+pymysql://root:328473@localhost/grupo_mar1'
-}
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-"""
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/grupo_mar'
-app.config['SQLALCHEMY_BINDS'] = {'estoque_producao':'mysql+pymysql://admin:226226uy@astrocode.cdee0eaoc82b.us-east-2.rds.amazonaws.com:3306/estoque_producao'}
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 
 
 #Informando o flask qual é minha pasta padrão para upload de arquivos
+UPLOAD_FOLDER = 'uploads'
 UPLOAD_FOLDER_FINANCEIRO = 'uploads/financeiro'
 ALLOWED_EXTENCIONS = ['csv']
 app.config['UPLOAD_FOLDER_FINANCEIRO'] = UPLOAD_FOLDER_FINANCEIRO
 #criando pasta caso não exista
 os.makedirs(UPLOAD_FOLDER_FINANCEIRO, exist_ok=True)
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 
-
+#Criação de esquemas (USADO APENAS PARA OTIMIZAR OS DEBUGS)
 def criar_schema(nome_schema):
     with app.app_context():
         db.session.execute(text(f"CREATE SCHEMA IF NOT EXISTS {nome_schema};"))
         db.session.commit()
 
+
+#(USADO APENAS PARA OTIMIZAR OS DEBUGS)
 def start_padrao():
     categorias = Nome_Categoria.query.all()
     nomes = Nome_Produto.query.all()
